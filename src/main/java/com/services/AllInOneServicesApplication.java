1d72c8686801fe106ba052dd9a8ed2f8041142c6
pavansrinivas.m <pavansrinivas.m@procreate.co.in>
2023-05-17 14:09:27 +0530

    first commit

---
+++ src/main/java/com/services/AllInOneServicesApplication.java
@@ -0,0 +1,13 @@
+package com.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllInOneServicesApplication.class, args);
+	}
+
+}
